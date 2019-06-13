b63d608013f9b9b0e0889ce8a01886ae476732db
pmtavares <pedrotavares41@gmail.com>
2019-06-13 21:38:04 +0100

    Seed Categories database

---
+++ MyPlace/Migrations/20190613203024_SeedDatabse.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyPlace.Migrations
+{
+    public partial class SeedDatabse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO CATEGORIES (Name) VALUES ('Technology')");
+            migrationBuilder.Sql("INSERT INTO CATEGORIES (Name) VALUES ('Science')");
+            migrationBuilder.Sql("INSERT INTO CATEGORIES (Name) VALUES ('Politics')");
+
+
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
